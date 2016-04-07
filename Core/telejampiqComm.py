6c8896294b63c207f6f62b904632615c9a26307b
Bregymr <bregy.malpartida@utec.edu.pe>
2016-04-06 22:12:45 -0500

    Communication core for connect the DB and mobile app

---
+++ Core/telejampiqComm.py
@@ -0,0 +1,27 @@
+
+
+from pymongo import MongoClient
+class pythonClientDB:
+
+    def __init__(self, server):
+        try:
+            self.client = MongoClient(server)
+        except:
+            print "No se pudo conectar"
+
+        self.databasesNames = self.client.database_names()
+
+    def readDBS(self):
+        return self.databasesNames
+
+    def getDB(self, database, collection):
+        vect = []
+        dataB = self.client[database]
+        coll = dataB[collection].find()
+
+        m = 0
+        for i in coll:
+            vect.apped(i)
+            m+=1
+        return vect
+
